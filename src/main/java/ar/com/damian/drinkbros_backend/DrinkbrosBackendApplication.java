30754088a74cd4fa21d05320184c99ccec91ae99
Damian <damianfernandez6720@gmail.com>
2024-08-20 20:20:05 -0300

    Application name

---
+++ src/main/java/ar/com/damian/drinkbros_backend/DrinkbrosBackendApplication.java
@@ -0,0 +1,13 @@
+package ar.com.damian.drinkbros_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkbrosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkbrosBackendApplication.class, args);
+	}
+
+}

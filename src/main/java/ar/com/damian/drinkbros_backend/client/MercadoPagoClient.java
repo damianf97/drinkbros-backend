aa02b14292efe6891a33c662fe57c4d52f26d322
damian <damianfernandez67@gmail.com>
2025-08-28 21:04:32 -0300

    inicio test v2

--- src/main/java/ar/com/damian/drinkbros_backend/client/MercadoPagoClient.java
+++ src/main/java/ar/com/damian/drinkbros_backend/client/MercadoPagoClient.java
@@ -10,16 +10,4 @@ public interface MercadoPagoClient {
     @POST("/oauth/token")
     @Headers({"Accept: application/json", "Content-Type: application/json"})
     Call<OauthTokenMPResponse> oauthToken(@Body OauthTokenMpRequest body);
-
-//    @POST("/v1/orders")
-//    @Headers({"Accept: application/json", "Content-Type: application/json"})
-//    Call<TmpResponseOrder> crearOrdenPoint(@Header("Authorization") String accessToken, @Header("X-Idempotency-Key") String idempotency, @Body TmpBodyOrder body);
-//
-//    @POST("/v1/orders/{order_id}/cancel")
-//    @Headers({"Accept: application/json", "Content-Type: application/json"})
-//    Call<TmpResponseOrder> cancelarOrdenPoint(@Header("Authorization") String accessToken, @Header("X-Idempotency-Key") String idempotency, @Path("order_id") String orderId);
-//
-//    @GET("/v1/orders/{order_id}")
-//    @Headers({"Accept: application/json", "Content-Type: application/json"})
-//    Call<TmpResponseOrder> buscarOrden(@Header("Authorization") String accessToken, @Path("order_id") String orderId);
 }
